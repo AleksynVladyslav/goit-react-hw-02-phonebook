afc180025de953a02b72592ef6eea5f05e5e31c4
Vladyslav Aleksyn <120416059+AleksynVladyslav@users.noreply.github.com>
2023-07-17 22:33:01 +0200

    update

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,60 +1,86 @@
 import React, { Component } from 'react';
-import css from './App.module.css';
-import Statistics from '../Statistics/Statistics';
-import FeedbackOptions from '../FeedbackOptions/FeedbackOptions';
-import Section from '../Section/Section';
-import Notification from '../Statistics/Notification';
+import shortid from 'shortid';
 
 class App extends Component {
   state = {
-    good: 0,
-    neutral: 0,
-    bad: 0,
+    contacts: [
+      { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
+      { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
+      { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
+      { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
+    ],
+    name: '',
+    number: '',
   };
 
-  onLeaveFeedback = event => {
-    const feedbackType = event.currentTarget.textContent.toLowerCase();
-    this.setState(prevState => ({
-      [feedbackType]: prevState[feedbackType] + 1,
-    }));
+  onAddContact = event => {
+    event.preventDefault();
+    const { name, number } = this.state;
+
+    if (name.trim() !== '' && number.trim() !== '') {
+      const newContact = {
+        id: shortid.generate(),
+        name: name.trim(),
+        number: number,
+      };
+
+      this.setState(prevState => ({
+        contacts: [...prevState.contacts, newContact],
+        name: '',
+        number: '',
+      }));
+    }
   };
 
-  countTotalFeedback() {
-    const total = this.state.good + this.state.neutral + this.state.bad;
-    return total;
-  }
+  onNameChange = event => {
+    this.setState({ name: event.target.value });
+  };
 
-  countPositiveFeedbackPercentage() {
-    return this.state.good !== 0
-      ? ((this.state.good / this.countTotalFeedback()) * 100).toFixed(0) + '%'
-      : '0';
-  }
+  onNumberChange = event => {
+    this.setState({ number: event.target.value });
+  };
 
   render() {
-    const { good, neutral, bad } = this.state;
-    const statisticsContent =
-      good === 0 && neutral === 0 && bad === 0 ? (
-        <Notification message={' No feedback given '} />
-      ) : (
-        <Statistics
-          good={good}
-          neutral={neutral}
-          bad={bad}
-          total={this.countTotalFeedback()}
-          positivePercentage={this.countPositiveFeedbackPercentage()}
-        />
-      );
+    const { contacts, name, number } = this.state;
 
     return (
-      <div className={css.app}>
-        <Section title={'Please leave feedback'}>
-          <FeedbackOptions
-            options={['good', 'neutral', 'bad']}
-            onLeaveFeedback={this.onLeaveFeedback}
-          />
-        </Section>
-
-        <Section title={'Statistics'}>{statisticsContent}</Section>
+      <div>
+        <h2>Phonebook</h2>
+        <form onSubmit={this.onAddContact}>
+          <label>
+            Name
+            <input
+              type="text"
+              name="name"
+              pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
+              title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
+              required
+              value={name}
+              onChange={this.onNameChange}
+            />
+          </label>
+          <label>
+            Number{' '}
+            <input
+              type="tel"
+              name="number"
+              pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
+              title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
+              required
+              value={number}
+              onChange={this.onNumberChange}
+            />
+          </label>
+          <button type="submit">Add Contact</button>
+        </form>
+        <h2>Contacts</h2>
+        <ul>
+          {contacts.map(contact => (
+            <li key={contact.id}>
+              {contact.name}: {contact.number}
+            </li>
+          ))}
+        </ul>
       </div>
     );
   }
